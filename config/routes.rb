9f11f5b62c59399bc51c8acaf0b0bb5d7fc2263f
Joel Hoelting <joelhoelting@protonmail.com>
2019-11-01 15:54:12 -0400

    Add index route for characters to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :games, only: [:create]
-      resources :characters, only: [:show]
+      resources :characters, only: [:index, :show]
       resources :quotes, only: [:show]
       get '/profile', to: 'auth#profile'
       post '/signup', to: 'auth#signup'
