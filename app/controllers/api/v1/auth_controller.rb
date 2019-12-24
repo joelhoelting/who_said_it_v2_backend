1ae8ffa33b25dad4504343fe74aa5640fd0878f9
Joel Hoelting <joelhoelting@protonmail.com>
2019-12-24 00:02:47 -0500

    Fine tune user credential strong params

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -7,7 +7,6 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def signup
-    binding.pry
     @user = User.create(user_credential_params)
     if @user.valid?
       @token = encode_token(:user_id => @user.id)
@@ -18,7 +17,6 @@ class Api::V1::AuthController < ApplicationController
   end
 
   def signin
-    binding.pry
     @user = User.find_by(:email => user_credential_params[:email])
     # authenticate method comes from bcrypt
     if @user && @user.authenticate(user_credential_params[:password])
@@ -32,6 +30,6 @@ class Api::V1::AuthController < ApplicationController
   private
 
   def user_credential_params
-    params.permit(:email, :password, :password_confirmation)
+    params.require(:auth).permit(:email, :password, :password_confirmation)
   end
 end
