bf49a836a9fb4c4a38cd2c825970f621490f2c11
Joel Hoelting <joelhoelting@protonmail.com>
2020-05-12 17:44:52 -0400

    Removes extraneous script

--- who_said_it_env_vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export RDS_DATABASE=whosaidit
-export RDS_USERNAME=postgres
-export RDS_PASSWORD=q83SenPQzrrKm2P
-export RDS_HOST=whosaidit.cviglyadx7ao.us-east-1.rds.amazonaws.com
-export RDS_PORT=5432
