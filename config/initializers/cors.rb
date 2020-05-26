dc5738d89ffc2ecc842d7c214bbda7aae51f1585
Joel Hoelting <joelhoelting@protonmail.com>
2020-05-25 23:44:12 -0400

    Updates CORS typo

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:8080, http://dev.whosaidit.co'
+    origins 'http://localhost:8080', 'http://dev.whosaidit.co'
 
     resource '*',
       headers: :any,
