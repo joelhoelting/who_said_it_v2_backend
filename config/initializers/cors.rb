a50f65e907fcdb599e21f248eb3637008dfccc15
Joel Hoelting <joelhoelting@protonmail.com>
2020-05-25 23:56:21 -0400

    Only let resources from /api/v1/*

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -9,7 +9,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:8080', 'http://dev.whosaidit.co'
 
-    resource '*',
+    resource '/api/v1/*',
       headers: :any,
       credentials: true,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
