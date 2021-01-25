90b6939f90b3c669a8f952c34afb609055c4f103
Joel Hoelting <joel@joelhoelting.com>
2021-01-25 11:55:01 -0500

    Updates cors allowed url

--- rails_app/config/initializers/cors.rb
+++ rails_app/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://whosaidit.co'
+    origins 'https://localhost:5000'
 
     resource '/api/v1/*',
              :headers => :any,
