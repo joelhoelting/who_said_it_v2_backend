dbe048ec47dc831e18cb9cdfade101ff80739a54
Joel Hoelting <joel@joelhoelting.com>
2021-01-26 00:30:52 -0500

    Updates cors whitelist with ENV variables

--- rails_app/config/initializers/cors.rb
+++ rails_app/config/initializers/cors.rb
@@ -10,7 +10,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:5000'
+    origins Rails.application.credentials[Rails.env.to_sym][:url]
 
     resource '/api/v1/*',
              :headers => :any,
